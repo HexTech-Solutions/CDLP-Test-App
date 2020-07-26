38cd7340b758087ec8b1091ff1d28bccf1ae07d5
charanaM <charana2010@gmail.com>
2020-07-26 14:47:42 +0530

    Implemented state selection screen

--- CDLPrepTest/app/src/main/java/com/hextech/cdlpreptest/MainActivity.java
+++ CDLPrepTest/app/src/main/java/com/hextech/cdlpreptest/MainActivity.java
@@ -1,14 +1,32 @@
 package com.hextech.cdlpreptest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements StateViewAdapter.OnStateListner {
+
+    RecyclerView recyclerView;
+    String s1[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.stateView);
+        s1 = getResources().getStringArray(R.array.state_names);
+
+        StateViewAdapter stateViewAdapter = new StateViewAdapter(this, s1);
+        recyclerView.setAdapter(stateViewAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    public void onStateClick(int position) {
+        Intent intent = new Intent();
     }
 }
