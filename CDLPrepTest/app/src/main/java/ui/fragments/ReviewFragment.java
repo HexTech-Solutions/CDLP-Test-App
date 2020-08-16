df067c095ec55be7a0347272171a3befb9cd8ee1
NisalM <nmmuthugala@gmail.com>
2020-08-16 23:44:52 +0530

    CLDP-9: Questions list screen in review option - dynamic one

--- CDLPrepTest/app/src/main/java/ui/fragments/ReviewFragment.java
+++ CDLPrepTest/app/src/main/java/ui/fragments/ReviewFragment.java
@@ -1,6 +1,7 @@
 package ui.fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,10 +15,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.hextech.cdlpreptest.QuestionStats;
 import com.hextech.cdlpreptest.R;
 import com.hextech.cdlpreptest.ReviewViewAdapter;
 
-public class ReviewFragment extends Fragment {
+public class ReviewFragment extends Fragment implements ReviewViewAdapter.OnReviweListener{
     View view;
     AdView adView;
     RecyclerView recyclerView;
@@ -36,7 +38,7 @@ public class ReviewFragment extends Fragment {
         reviewQuestionCount = new String[]{"0 questions", "0 questions", "0 questions", "0 questions", "0 questions"};
         context = container.getContext();
 
-        ReviewViewAdapter reviewViewAdapter = new ReviewViewAdapter(context, reviewTitle, reviewQuestionCount, icons);
+        ReviewViewAdapter reviewViewAdapter = new ReviewViewAdapter(context, reviewTitle, reviewQuestionCount, icons, this);
         recyclerView.setAdapter(reviewViewAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(context));
 
@@ -47,5 +49,11 @@ public class ReviewFragment extends Fragment {
         adView = view.findViewById(R.id.adView);
         AdRequest request = new AdRequest.Builder().build();
         adView.loadAd(request);
+    }
+
+    @Override
+    public void onReviewClick(int position) {
+        Intent intent = new Intent(context, QuestionStats.class);
+        startActivity(intent);
     }
 }
